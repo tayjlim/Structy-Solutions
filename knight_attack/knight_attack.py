07944fbdf78612a1f592956984cbf86cb27030b9
Taylor Lim <120624931+tayjlim@users.noreply.github.com>
2023-12-12 14:04:57 -0800

    Solved 'knight attack' (.py)

--- knight_attack/knight_attack.py
+++ knight_attack/knight_attack.py
@@ -1,50 +1,36 @@
-
 from collections import deque
 ​
 def knight_attack(n, kr, kc, pr, pc):
-  visited = set() # make sure we dont back track 
-  visited.add((kr,kc))
-  queue = deque([(kr, kc, 0)])  # Correctly initialize the deque here
-  while len(queue) > 0:
-    # CURRENT 
-    r,c,steps = queue.popleft();
-    if (r,c) == (pr,pc):
-      return steps
-    
-    moves = nextMoves(n,r,c)
-    for move in moves:
-      x,y = move
-      
-      if move not in visited:
-        visited.add(move)
-        queue.append((x,y,steps+1))
-  return None
+    # Edge cases:
+        #if all combinations tried, return None
+        #if board size is less < than 2, return None (Knight cant move less than 2 vertically)
+        #if knight position and pawn position are the same, return None since its already attacking
+    if n < 2 or (kr, kc) == (pr, pc):
+        return None
 ​
-  # deconstruct this because we are only x,y 
-    
-def nextMoves (n,r,c):
-  moves= []
-  positions = [
+    #?I guess use a queue
+    queue = deque([(kr, kc, 0)])
+    #?Save what was visited in a set, lookup condition to stop it from returning
+    visited = set()
+​
+    #Some DFS
+    #Knight can only move horizontally +/- 1 and vertically +/- 2 (8 total possibilities)
+    moves = [(-2, -1), (-1, -2), (1, -2), (2, -1), (-2, 1), (-1, 2), (1, 2), (2, 1)]
+​
+    while queue:
+        #Keep start position of knight with dist 0
+        #get front element of queue, which is (r, c) that knight is going to
+        #loop to check all possible moves from current position
+        row, col, dist = queue.popleft()
+        
+        #Add a check if the knight and the pawn are on the same square (already attacking)
+        if (row, col) == (pr, pc):
+            return dist
+       
+        for delta_row, delta_col in moves:
+            new_row, new_col = row + delta_row, col + delta_col
+            if 0 <= new_row < n and 0 <= new_col < n and (new_row, new_col) not in visited:
+                visited.add((new_row, new_col))
+                queue.append((new_row, new_col, dist+1))
+    return None
   (r+2,c+1), # xy (8 directions 2 up down left right, 1 up down left right)
-  (r-2,c+1), 
-  (r+2,c-1),
-  (r-2,c-1),
-  (r-1,c+2),
-  (r+1,c-2),
-  (r-1,c-2),
-  (r+1,c+2)]
-  
-  for pos in positions:
-    new_row, new_col = pos
-    # IF IT IS IN BOUNDS 
-    if 0<= new_row < n and 0<= new_col < n:
-      moves.append(pos)
-​
-  
-  return moves
-​
-​
-#2 left 
-# 2 right
-#2up
-# n is tize of graph
