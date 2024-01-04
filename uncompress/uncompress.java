cbdc91f71b5376ee071e8f0d1a28b4e3729bf834
Taylor Lim <120624931+tayjlim@users.noreply.github.com>
2024-01-03 17:14:05 -0800

    Solved 'uncompress' (.java)

--- uncompress/uncompress.java
+++ uncompress/uncompress.java
@@ -5,18 +5,23 @@ class Source {
     int i = 0;
     int j = 0;
     while (j < s.length()) {
-      if (numbers.contains(String.valueOf(s.charAt(j)))) {//if we hit we add to j
+      System.out.println(numbers.contains(String.valueOf(s.charAt(j))));
+      if (numbers.contains(String.valueOf(s.charAt(j))) == true) {//if we hit we add to j
+       
         j += 1;
-        System.out.println(j);
+        System.out.println(j + " this is j");
       } else {// if we hit a char
         int num = Integer.parseInt(s.substring(i, j));
-        System.out.println(num);
+        System.out.println(num + " this is num");
         
-        for (int count = 0; count < num; count += 1) {
-          result += s.charAt(j);
-        }
+          for (int count = 0; count < num; count += 1) {
+            result += s.charAt(j);
+          }
         j += 1;
         i = j;
+        System.out.println(i + " this is i");
+​
+        
       }
     }
     return result;
